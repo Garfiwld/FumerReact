5341fa7a9ba4d89fa05a86bf27d65ca47b5d9176
GarFiwld <garfiwld@icloud.com>
2020-03-24 01:09:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import clsx from "clsx";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
+import AppMenu from "./AppMenu";
+
+import BisectionMethod from "./01-ROOT-OF-EQUATION/Bisection-Method";
+
+const App: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={clsx("App", classes.root)}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <AppMenu />
+        </Drawer>
+        <main className={classes.content}>
+          <Container maxWidth="lg" className={classes.container}>
+            <Switch>
+              <Route path="/" exact component={BisectionMethod} />
+              <Route path="/Bisection-Method" component={BisectionMethod} />
+            </Switch>
+          </Container>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const drawerWidth = 300;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    background: "#535454",
+    color: "#fff"
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  }
+}));
+
+export default App;
---
+++ src/AppMenu.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+import List from "@material-ui/core/List";
+
+import Filter1Icon from "@material-ui/icons/Filter1";
+import IconShoppingCart from "@material-ui/icons/ShoppingCart";
+import IconPeople from "@material-ui/icons/People";
+import IconBarChart from "@material-ui/icons/BarChart";
+import IconLibraryBooks from "@material-ui/icons/LibraryBooks";
+
+import AppMenuItem from "./AppMenuItem";
+import Item from "antd/lib/list/Item";
+
+import "./css/AppMenu.css";
+
+const appMenuItems = [
+  {
+    name: "ROOT OF EQUATION",
+    Icon: Filter1Icon,
+    items: [
+      {
+        name: "BisectionMethod",
+        link: "/Bisection-Method"
+      },
+      {
+        name: "False position Method",
+        link: "/False-position-Method"
+      },
+      {
+        name: "One-point iteration Method",
+        link: "/One-point-iteration-Method"
+      },
+      {
+        name: "Newton raphson Method",
+        link: "/Newton-raphson-Method"
+      },
+      {
+        name: "Secant Method",
+        link: "/Secant-Method"
+      }
+    ]
+  },
+  {
+    name: "Orders",
+    link: "/orders",
+    Icon: IconShoppingCart
+  },
+  {
+    name: "Customers",
+    link: "/customers",
+    Icon: IconPeople
+  },
+  {
+    name: "Reports",
+    link: "/reports",
+    Icon: IconBarChart
+  },
+  {
+    name: "Nested Pages",
+    Icon: IconLibraryBooks,
+    items: [
+      {
+        name: "Level 2"
+      },
+      {
+        name: "Level 2",
+        items: [
+          {
+            name: "Level 3"
+          },
+          {
+            name: "Level 3"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const AppMenu: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <List component="nav" className={classes.appMenu} disablePadding>
+      {/* <AppMenuItem {...appMenuItems[0]} /> */}
+      {appMenuItems.map((item, index) => (
+        <AppMenuItem {...item} key={index} />
+      ))}
+    </List>
+  );
+};
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    appMenu: {
+      width: "100%"
+    },
+    navList: {
+      width: drawerWidth
+    },
+    menuItem: {
+      width: drawerWidth
+    },
+    menuItemIcon: {
+      color: "#97c05c"
+    }
+  })
+);
+
+export default AppMenu;
