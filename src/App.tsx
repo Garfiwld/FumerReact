047787dd58eb2f667d166843327a72c85478e759
GarFiwld <garfiwld@icloud.com>
2020-03-25 17:49:18 +0700

    Hi Deadline !!!

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,37 @@ import Container from "@material-ui/core/Container";
 
 import AppMenu from "./AppMenu";
 
-import BisectionMethod from "./01-ROOT-OF-EQUATION/Bisection-Method";
-import FalsePositionMethod from "./01-ROOT-OF-EQUATION/False_position";
+import Bisection from './Root of Equation/Bisection'
+import False from './Root of Equation/False_position';
+import Onepoint from './Root of Equation/Onepoint';
+import Newton from './Root of Equation/Newton-raphson';
+import Secant from './Root of Equation/Secant';
+import Cramer from './Linear Algebra/Cramer';
+import Gauss from './Linear Algebra/Gauss';
+import Jordan from './Linear Algebra/Jordan';
+import Inverse from './Linear Algebra/Inverse';
+import LU from './Linear Algebra/LU';
+import Cholesky from './Linear Algebra/Cholesky';
+import Jacobi from './Linear Algebra/Jacobi';
+import Seidel from './Linear Algebra/Seidel';
+import Gradient from './Linear Algebra/Gradient';
+import NewtonInterpolate from './Interpolation/Newton';
+import Lagrange from './Interpolation/Lagrange';
+import Spline from './Interpolation/Spline';
+import Linear from './Regression/Linear';
+import Polynomial from './Regression/Polynomial';
+import MultipleLinear from './Regression/MultipleLinear';
+import CompositeTrapezoidal from './Integration/CompositeTrapzoidal';
+import CompositeSimpson from './Integration/CompositeSimpson';
+import ForwardH from './Differentiation/Forwardh';
+import ForwardH2 from './Differentiation/ForwardH2';
+import BackwardH from './Differentiation/Backwardh';
+import BackwardH2 from './Differentiation/Backwardh2';
+import CentralH from './Differentiation/Centralh';
+import CentralH2 from './Differentiation/Centralh2';
+import Euler from './ODE/Euler';
+import Heun from './ODE/Heun';
+import ModifiedEuler from './ODE/Modified_Euler';
 
 const App: React.FC = () => {
   const classes = useStyles();
@@ -29,12 +58,39 @@ const App: React.FC = () => {
         <main className={classes.content}>
           <Container maxWidth="lg" className={classes.container}>
             <Switch>
-              <Route path="/" exact component={BisectionMethod} />
-              <Route path="/Bisection-Method" component={BisectionMethod} />
-              <Route
-                path="/False-position-Method"
-                component={FalsePositionMethod}
-              />
+              <Route path="/" exact component={Bisection} />
+              <Route path="/Bisection" component={Bisection} />
+              <Route path="/False" component={False} />
+              <Route path="/Onepoint" component={Onepoint} />
+              <Route path="/Newton" component={Newton} />
+              <Route path="/Secant" component={Secant} />
+              <Route path="/Cramer" component={Cramer} />
+              <Route path="/Gauss" component={Gauss} />
+              <Route path="/Jordan" component={Jordan} />
+              <Route path="/Inverse" component={Inverse} />
+              <Route path="/LU" component={LU} />
+              <Route path="/Cholesky" component={Cholesky} />
+              <Route path="/Jacobi" component={Jacobi} />
+              <Route path="/Seidel" component={Seidel} />
+              <Route path="/Gradient" component={Gradient} />
+              <Route path="/NewtonInterpolate" component={NewtonInterpolate} />
+              <Route path="/Lagrange" component={Lagrange} />
+              <Route path="/Spline" component={Spline} />
+              <Route path="/Linear" component={Linear} />
+              <Route path="/Polynomial" component={Polynomial} />
+              <Route path="/MultipleLinear" component={MultipleLinear} />
+              <Route path="/CompositeTrapezoidal" component={CompositeTrapezoidal} />
+              <Route path="/CompositeSimpson" component={CompositeSimpson} />
+              <Route path="/ForwardH" component={ForwardH} />
+              <Route path="/ForwardH2" component={ForwardH2} />
+              <Route path="/BackwardH" component={BackwardH} />
+              <Route path="/BackwardH2" component={BackwardH2} />
+              <Route path="/CentralH" component={CentralH} />
+              <Route path="/CentralH2" component={CentralH2} />
+              <Route path="/Euler" component={Euler} />
+              <Route path="/Heun" component={Heun} />
+              <Route path="/ModifiedEuler" component={ModifiedEuler} />
+
             </Switch>
           </Container>
         </main>
--- src/AppMenu.tsx
+++ src/AppMenu.tsx
@@ -4,10 +4,12 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 
 import Filter1Icon from "@material-ui/icons/Filter1";
-import IconShoppingCart from "@material-ui/icons/ShoppingCart";
-import IconPeople from "@material-ui/icons/People";
-import IconBarChart from "@material-ui/icons/BarChart";
-import IconLibraryBooks from "@material-ui/icons/LibraryBooks";
+import Filter2Icon from "@material-ui/icons/Filter2";
+import Filter3Icon from "@material-ui/icons/Filter3";
+import Filter4Icon from "@material-ui/icons/Filter4";
+import Filter5Icon from "@material-ui/icons/Filter5";
+import Filter6Icon from "@material-ui/icons/Filter6";
+import Filter7Icon from "@material-ui/icons/Filter7";
 
 import AppMenuItem from "./AppMenuItem";
 // import Item from "antd/lib/list/Item";
@@ -20,62 +22,175 @@ const appMenuItems = [
     Icon: Filter1Icon,
     items: [
       {
-        name: "BisectionMethod",
-        link: "/Bisection-Method"
+        name: "Bisection",
+        link: "/Bisection"
       },
       {
-        name: "False position Method",
-        link: "/False-position-Method"
+        name: "False",
+        link: "/False"
       },
       {
-        name: "One-point iteration Method",
-        link: "/One-point-iteration-Method"
+        name: "Onepoint",
+        link: "/Onepoint"
       },
       {
-        name: "Newton raphson Method",
-        link: "/Newton-raphson-Method"
+        name: "Newton",
+        link: "/Newton"
       },
       {
-        name: "Secant Method",
-        link: "/Secant-Method"
-      }
+        name: "Secant",
+        link: "/Secant"
+      },
     ]
   },
   {
-    name: "Orders",
-    link: "/orders",
-    Icon: IconShoppingCart
-  },
-  {
-    name: "Customers",
-    link: "/customers",
-    Icon: IconPeople
-  },
-  {
-    name: "Reports",
-    link: "/reports",
-    Icon: IconBarChart
-  },
-  {
-    name: "Nested Pages",
-    Icon: IconLibraryBooks,
+    name: "Linear Algebra",
+    Icon: Filter2Icon,
     items: [
       {
-        name: "Level 2"
+        name: "Cramer",
+        link: "/Cramer"
       },
       {
-        name: "Level 2",
-        items: [
-          {
-            name: "Level 3"
-          },
-          {
-            name: "Level 3"
-          }
-        ]
+        name: "Gauss",
+        link: "/Gauss"
+      },
+      {
+        name: "Jordan",
+        link: "/Jordan"
+      },
+      {
+        name: "Inverse",
+        link: "/Inverse"
+      },
+      {
+        name: "LU",
+        link: "/LU"
+      },
+      {
+        name: "Cholesky",
+        link: "/Cholesky"
+      },
+      {
+        name: "Jacobi",
+        link: "/Jacobi"
+      },
+      {
+        name: "Seidel",
+        link: "/Seidel"
+      },
+      {
+        name: "Gradient",
+        link: "/Gradient"
       }
     ]
-  }
+  },
+  {
+    name: "Interpolation",
+    Icon: Filter3Icon,
+    items: [
+      {
+        name: "Newton",
+        link: "/Newton"
+      },
+      {
+        name: "Lagrange",
+        link: "/Lagrange"
+      },
+      {
+        name: "Spline",
+        link: "/Spline"
+      },
+      {
+        name: "Spline",
+        link: "/Inverse"
+      }
+    ]
+  },
+  {
+    name: "Regression",
+    Icon: Filter4Icon,
+    items: [
+      {
+        name: "Linear",
+        link: "/Linear"
+      },
+      {
+        name: "Polynomial",
+        link: "/Polynomial"
+      },
+      {
+        name: "MultipleLinear",
+        link: "/MultipleLinear"
+      }
+    ]
+  },
+  {
+    name: "Integration",
+    Icon: Filter5Icon,
+    items: [
+      {
+        name: "CompositeTrapzoidal",
+        link: "/CompositeTrapzoidal"
+      },
+      {
+        name: "CompositeSimpson",
+        link: "/CompositeSimpson"
+      }
+    ]
+  },
+  {
+    name: "Differentiation",
+    Icon: Filter6Icon,
+    items: [
+      {
+        name: "Forwardh",
+        link: "/Forwardh"
+      },
+      {
+        name: "ForwardH2",
+        link: "/ForwardH2"
+      },
+      {
+        name: "ForwardH2",
+        link: "/ForwardH2"
+      },
+      {
+        name: "Backwardh",
+        link: "/Backwardh"
+      },
+      {
+        name: "Backwardh2",
+        link: "/Backwardh2"
+      },
+      {
+        name: "Centralh",
+        link: "/Centralh"
+      },
+      {
+        name: "Centralh2",
+        link: "/Centralh2"
+      }
+    ]
+  },
+  {
+    name: "ODE",
+    Icon: Filter7Icon,
+    items: [
+      {
+        name: "Euler",
+        link: "/Euler"
+      },
+      {
+        name: "Heun",
+        link: "/Heun"
+      },
+      {
+        name: "Modified_Euler",
+        link: "/Modified_Euler"
+      }
+    ]
+  },
 ];
 
 const AppMenu: React.FC = () => {
